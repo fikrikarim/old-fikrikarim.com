4454a34701c8373655d339ec8c36be4ed483abf8
Fikri Karim <fikri@makeswift.com>
2021-04-02 05:58:56 +0700

    Update CV and move old CV into separate year

---
+++ src/pages/cv_2018.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Helmet from 'react-helmet';
+
+class Cv extends React.Component {
+  render() {
+    return (
+      <Helmet>
+        <meta
+          http-equiv="refresh"
+          content={'0;url=/Resume_of_Fikri_Karim_2018.pdf'}
+        />
+      </Helmet>
+    );
+  }
+}
+export default Cv;
